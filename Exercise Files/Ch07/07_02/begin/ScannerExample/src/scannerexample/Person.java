22b74f6d82383c87cb477a9932d5a47f2e9d7595
Luis Molina <luisfmt@ext.inditex.com>
2022-06-15 11:34:19 -0300

    i/o streams

--- Exercise Files/Ch07/07_02/begin/ScannerExample/src/scannerexample/Person.java
+++ Exercise Files/Ch07/07_02/begin/ScannerExample/src/scannerexample/Person.java
@@ -6,21 +6,20 @@
 package scannerexample;
 
 /**
- *
- * @author betha
+ * @author luis
  */
 public class Person {
-    
+
     private String name;
     private int age;
     private long phoneNumber;
-    
+
     public Person(String name, int age, long phoneNumber) {
         this.name = name;
         this.age = age;
         this.phoneNumber = phoneNumber;
-        System.out.println("Person Created. Name: " 
+        System.out.println("Person Created. Name: "
                 + name + ", Age: " + age + ", phone number: " + phoneNumber);
     }
-    
+
 }
--- Exercise Files/Ch07/07_02/begin/ScannerExample/src/scannerexample/PersonCreator.java
+++ Exercise Files/Ch07/07_02/begin/ScannerExample/src/scannerexample/PersonCreator.java
@@ -1,17 +1,31 @@
 
 package scannerexample;
 
+import java.util.Scanner;
+
 /**
- *
- * @author bethan
+ * @author luis
  */
 public class PersonCreator {
-    
+
     public static void main(String[] args) {
-        
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("enter the name: ");
+        String name = scanner.next();
+
+        System.out.println("enter the age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("enter the phone number: ");
+        Long phoneNumber = scanner.nextLong();
+
+//        System.out.println("Enter the name, age and phone number: ");
+//        String name = scanner.next();
+//        int age = scanner.nextInt();
+//        Long phoneNumber = scanner.nextLong();
         
+        Person person = new Person(name, age, phoneNumber);
     }
-    
 }
